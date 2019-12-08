d8aa57b4cc62707170b1275b6c985750476ef474
stephan <stephan.staeheli@gmx.ch>
2019-12-08 17:59:02 +0100

    remove unnessecary logs 2

--- src/main/java/ch/basler/playground/kafka/KafkaProducer.java
+++ src/main/java/ch/basler/playground/kafka/KafkaProducer.java
@@ -2,8 +2,6 @@ package ch.basler.playground.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import ch.basler.playground.kafka.model.RssFeedEntry;
@@ -14,8 +12,8 @@ public class KafkaProducer {
   private static final Logger LOG = LoggerFactory.getLogger(KafkaProducer.class);
   private static final String TOPIC = "heise";
 
-  @Autowired
-  private KafkaTemplate<String, RssFeedEntry> kafkaTemplate;
+//  @Autowired
+//  private KafkaTemplate<String, RssFeedEntry> kafkaTemplate;
 
   public void sendMessage(RssFeedEntry message) {
     LOG.info(String.format("#### -> Producing message -> %s", message));
